1e2a085d43fc06e2b0cbcf7aecc3c74f3150a0fa
Kevin K <kithinjimkevin@gmail.com>
2020-02-04 15:59:25 +0300

    removed dummy content

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Each class is a table in the database (eg. Post)
+# Each attribute is a field in the database (eg title) 
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # delete post if user is deleted
+
+    def __str__(self):
+        return(self.title)
+
