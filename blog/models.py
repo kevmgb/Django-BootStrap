9dcf55789cec86d8fa111b81359b5b0e9e15e26c
Kevin K <kithinjimkevin@gmail.com>
2020-02-05 13:57:02 +0300

    User creation Forms

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Each class is a table in the database (eg. Post)
 # Each attribute is a field in the database (eg title) 
@@ -14,3 +15,5 @@ class Post(models.Model):
     def __str__(self):
         return(self.title)
 
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
